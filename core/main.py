49c889e9fc610bbaa362b24b50c046721a3a4bae
cesar <cesar@petrolog.us>
2019-07-30 16:10:38 -0600

    Initial serial test

--- core/main.py
+++ core/main.py
@@ -1,21 +1,56 @@
 
-import serial
-
 import sys
+import serial
 import time
-
-import pendulum
 
 
 print(sys.version_info)
+port = serial.Serial("/dev/ttyUSB0", baudrate=19200, timeout=1)
 
-errors = 0
+
+def __get_command(command):
+    """
+    Sends and returns the result of a G4 command
+    :param command: command to send, without address or CR
+    :return: response from ESC. can be timeout.
+    """
+    address = 1
+    port.flushInput()
+    port.flushOutput()
+    port.flush()
+
+    to_send = "{:02d}{}\x0D".format(address, command)
+    port.write(to_send.encode())
+    print(('g4_esc_petrolog: __get_command - Tx: {0}'.format(to_send)))
+
+    rx = port.readline().decode()
+    if rx == '':
+        print('g4_esc_petrolog: __get_command - Timeout!')
+        return "Timeout"
+    elif rx[2] == command[0]:
+        to_return = rx[:-1]
+        print(('g4_esc_petrolog: __get_command - Success Rx: {0}'.format(to_return)))
+        return to_return
+    else:
+        print(('g4_esc_petrolog: __get_command - Ugly trash! = {0}'.format(rx)))
+
+
 while True:
     try:
-        print('Core container running @ {0}'.format(pendulum.now('America/Chihuahua')))
-    except Exception as e:
-        errors += 1
-        print('In while - Error: {0}'.format(e.__str__()))
+        mb = __get_command('MB')
+        time.sleep(.1)
+        print("Response from G4 - MB: [{0}]".format(mb))
 
-    time.sleep(10)
-    continue
+        mb = __get_command('S?1')
+        time.sleep(.1)
+        print("Response from G4 - S?1: [{0}]".format(mb))
+
+        mb = __get_command('H')
+        time.sleep(.1)
+        print("Response from G4 - H: [{0}]".format(mb))
+
+    except ValueError as e:
+        print("ValueError: {0}".format(e))
+    except IOError as e:
+        print("IOError: {0}".format(e))
+    time.sleep(2)
