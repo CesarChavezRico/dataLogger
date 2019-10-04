ac7518e7b2e29e31311eaf4421fe18effdc7be7c
cesar <cesar@petrolog.us>
2019-10-04 12:57:11 -0600

    Major: Added Lock for accessing running file in permanent USB - Missing parameter to object init

--- core/main.py
+++ core/main.py
@@ -15,16 +15,14 @@ else:
     running_file_lock = threading.Lock()
 
     # launch USB Drive scanning and file backup process
-    usb_storage = USB()
-    scan_and_backup = threading.Thread(target=usb_storage.check_for_devices,
-                                       args=[running_file_lock])
+    usb_storage = USB(running_file_lock)
+    scan_and_backup = threading.Thread(target=usb_storage.check_for_devices)
     scan_and_backup.daemon = True
     scan_and_backup.start()
 
     # launch polling to serial device
-    serial_g4_device = G4()
-    polling = threading.Thread(target=serial_g4_device.serial_polling,
-                               args=[running_file_lock])
+    serial_g4_device = G4(running_file_lock)
+    polling = threading.Thread(target=serial_g4_device.serial_polling)
     polling.daemon = True
     polling.start()
 
